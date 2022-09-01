bd0aab23c132f83a90edd6771cbbef885ce505ca
kamil098 <kamilbombe855@gmail.com>
2022-09-01 20:01:08 +0530

    Day4 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,16 +4,58 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              height: 300,
+              width: 300,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome To Docpad",
+              style: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter a User Name",
+                      labelText: "User Name",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter a Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Hi Docpad");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
